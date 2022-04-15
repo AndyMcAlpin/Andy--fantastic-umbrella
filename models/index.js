9c1f3ec3d5db5920e49efbc79fc27001d59c49a4
Andy MCA <AndrewMcAlpin@gmail.com>
2022-04-15 10:48:32 -0500

    initial commit with models

---
+++ models/index.js
@@ -0,0 +1,33 @@
+// import models
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
+
+// Products belongsTo 
+Product.belongsTo(Category, {
+  foreignKey: 'id'
+});
+// Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'id'
+});
+// Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  as: 'taged_products',
+  foreignKey: 'product_id'
+});
+// Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  as: 'taged_products',
+  foreignKey: 'tag_id'
+});
+
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
